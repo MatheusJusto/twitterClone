fc5681fc1def97d2c7385bbbd60ad3d4cdf4cec0
Matheus Garcia <matheusgarciadg@gmail.com>
2017-06-01 01:47:03 -0300

    finalizado o projeto

--- get_pessoa.php
+++ get_pessoa.php
@@ -13,20 +13,38 @@
     $objDb = new db();
     $link = $objDb->conecta_mysql();
 
-    $sql = " SELECT * FROM usuarios WHERE usuario like '%$nome_pessoa%' AND id <> $id_usuario ";
+    $sql = " SELECT u.*, us.* ";
+    $sql .= " FROM usuarios AS u ";
+    $sql .= " LEFT JOIN usuarios_seguidores AS us";
+    $sql .= " ON (us.id_usuario = $id_usuario AND u.id = us.seguindo_id_usuario)";
+    $sql .= " WHERE u.usuario like '%$nome_pessoa%' AND u.id <> $id_usuario";
 
     $resultado_id = mysqli_query($link, $sql);
 
     if($resultado_id) {
         
         while($registro = mysqli_fetch_array($resultado_id, MYSQLI_ASSOC)) {
-            echo "<a href='#' class='list-group-item'>";
-                echo "<strong>".$registro['usuario']."</strong> <small> -".$registro['email']." </small>";
-                echo "<p class='list-group-item-tex pull-right'>";
-                    echo "<button type='button' class='btn btn-default'>Seguir</button>";
-                echo "</p>";
-                echo "<div class='clearfix'></div>";
-            echo "</a>";
+            echo '<a href="#" class="list-group-item">';
+                echo '<strong>'.$registro['usuario'].'</strong> <small> -'.$registro['email'].'</small>';
+                echo '<p class="list-group-item-tex pull-right">';
+
+                $esta_seguindo_usuario =  isset($registro['id_usuario_seguidor']) && !empty($registro['id_usuario']) ? 'Y' : 'N';
+
+                $btn_seguir_display = 'block';
+                $btn_deixar_seguir_display = 'block';
+
+                if($esta_seguindo_usuario == 'N') {
+                    $btn_deixar_seguir_display = 'none';
+                }
+                else {
+                    $btn_seguir_display = 'none';
+                }
+
+                    echo '<button type="button" id="btn-seguir-'.$registro['id'].'"  style="display: '.$btn_seguir_display.';" class="btn btn-default btn-seguir" data-id_usuario="'.$registro['id'].'">Seguir</button>';
+					echo '<button type="button" id="btn-deixar-seguir-'.$registro['id'].'" style="display: '.$btn_deixar_seguir_display.';" class="btn btn-primary btn-deixar-seguir" data-id_usuario="'.$registro['id'].'">Deixar de Seguir</button>';
+                echo '</p>';
+                echo '<div class="clearfix"></div>';
+            echo '</a>';
         }
     }
     else {
--- search.js
+++ search.js
@@ -8,7 +8,47 @@ $( 'document' ).ready( function () {
                 method: 'post',
                 data: $( '#form_procurar_pessoas' ).serialize(),
                     success: function (data) {
-                        alert($( '#pessoas' ).html(data));
+                        $( '#pessoas' ).html(data);
+
+
+                        // F O L L O W
+
+                        $( '.btn-seguir' ).click( function() {
+                            var id_usuario = $(this).data('id_usuario');
+
+                            $('#btn-seguir-'+id_usuario).hide();
+                            $('#btn-deixar-seguir-'+id_usuario).show();
+
+
+                            $.ajax( {
+                                url: 'seguir.php',
+                                method: 'post',
+                                data: { seguir_id_usuario: id_usuario},
+                                success: function (data) {
+                                    alert( 'Registro efetuado com sucesso' );
+                                }
+                            });
+                        });
+
+                        // U N F O L L O W
+
+                        $( '.btn-deixar-seguir' ).click( function() {
+                            var id_usuario = $(this).data('id_usuario');
+
+                            $('#btn-seguir-'+id_usuario).show();
+                            $('#btn-deixar-seguir-'+id_usuario).hide();
+
+                            $.ajax( {
+                                url: 'deixar_seguir.php',
+                                method: 'post',
+                                data: { deixar_seguir_id_usuario: id_usuario},
+                                success: function (data) {
+                                    alert( 'Registro removido com sucesso' );
+                                }
+                            });
+                        });
+
+
                     }
             });
         }
